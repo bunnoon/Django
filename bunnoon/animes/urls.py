4aeb804b1001e375eed64e40fba28b48fca0113a
Phuchon Chueapong <140912336+bunnoon@users.noreply.github.com>
2023-07-30 05:39:57 +0700

    setup animes app
    create list anime view and template
    create view anime view and template

--- bunnoon/animes/urls.py
+++ bunnoon/animes/urls.py
@@ -1,3 +1,8 @@
-from django.urls import path
+from django.urls import re_path, path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.animes, name='list-anime'),
+    re_path(r'^(?P<pk>\w{0,7})/(?P<title>\w{0,50})$',
+            views.anime, name='view-anime'),
+]
--- bunnoon/animes/views.py
+++ bunnoon/animes/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def animes(request: HttpRequest):
+    return render(request, 'ListAnime.html', {'title': 'Anime List Page'})
+
+
+def anime(request: HttpRequest, pk: str, title: str):
+    return render(request, 'ViewAnime.html', {'title': 'Anime View Page'})
