87e6503baae85be99a23fda3f4bd474793a71837
Phuchon Chueapong <140912336+bunnoon@users.noreply.github.com>
2023-07-31 19:49:11 +0700

    update animes app
    update list anime view and template
    update view anime view and template

--- bunnoon/animes/urls.py
+++ bunnoon/animes/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.animes, name='list-anime'),
-    re_path(r'^(?P<pk>\w{0,7})/(?P<title>\w{0,50})$',
+    re_path(r'^(?P<pk>\w{7})/(?P<title>\S{1,60})$',
             views.anime, name='view-anime'),
 ]
--- bunnoon/animes/views.py
+++ bunnoon/animes/views.py
@@ -1,12 +1,31 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from .models import Anime
 
 # Create your views here.
 
 
 def animes(request: HttpRequest):
-    return render(request, 'ListAnime.html', {'title': 'Anime List Page'})
+    queryset = Anime.objects.all()
+    return render(request, 'ListAnime.html', {
+        'success': True,
+        'status': 200,
+        'title': 'Anime List Page',
+        'data': queryset,
+    })
 
 
 def anime(request: HttpRequest, pk: str, title: str):
-    return render(request, 'ViewAnime.html', {'title': 'Anime View Page'})
+    try:
+        queryset = Anime.objects.get(animePk=pk)
+        return render(request, 'ViewAnime.html', {
+            'success': True,
+            'status': 200,
+            'title': 'Anime View Page',
+            'data': queryset,
+        })
+    except Anime.DoesNotExist:
+        return render(request, 'ViewAnime.html', {
+            'success': False,
+            'status': 404,
+        })
