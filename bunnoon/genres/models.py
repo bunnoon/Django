d407aa0e1dc062f2c613941b0d3851edf0b3dd8d
Phuchon Chueapong <140912336+bunnoon@users.noreply.github.com>
2023-07-31 20:27:40 +0700

    create genre model into genres app

--- bunnoon/genres/models.py
+++ bunnoon/genres/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(null=True, blank=True)
+    deletedAt = models.DateTimeField(null=True, blank=True)
+    genrePk = models.CharField(max_length=40, unique=True)
+    enName = models.CharField(max_length=40)
+    thName = models.CharField(max_length=40)
