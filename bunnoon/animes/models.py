def53588c96420e53fee79798c816a7ab6a2b36c
Phuchon Chueapong <140912336+bunnoon@users.noreply.github.com>
2023-07-31 19:45:38 +0700

    create anime model into animes app

--- bunnoon/animes/models.py
+++ bunnoon/animes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Anime(models.Model):
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(null=True, blank=True)
+    deletedAt = models.DateTimeField(null=True, blank=True)
+    animePk = models.CharField(max_length=7, unique=True)
+    title = models.CharField(max_length=60)
+    enName = models.CharField(max_length=60)
+    thName = models.CharField(max_length=60)
