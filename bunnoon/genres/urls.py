f913d6d1d6d671619d91c44ec9eb2fa2b4dbb02e
Phuchon Chueapong <140912336+bunnoon@users.noreply.github.com>
2023-07-31 20:22:45 +0700

    setup genres app
    create list genre view and template
    create view genre view and template

--- bunnoon/genres/urls.py
+++ bunnoon/genres/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import re_path, path
 from . import views
 
 urlpatterns = [
+    path('', views.genres, name='list-genre'),
+    re_path(r'^(?P<pk>\S{1,50})$', views.genre, name='view-genre'),
 ]
--- bunnoon/genres/views.py
+++ bunnoon/genres/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def genres(request: HttpRequest):
+    return render(request, 'ListGenre.html', {'title': 'Genre List Page'})
+
+
+def genre(request: HttpRequest, pk: str):
+    return render(request, 'ViewGenre.html', {'title': 'Genre View Page'})
