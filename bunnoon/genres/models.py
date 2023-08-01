6e00dd11830d57c36fe999ee0c76f1b14f675c1c
Phuchon Chueapong <140912336+bunnoon@users.noreply.github.com>
2023-08-01 16:54:34 +0700

    create genres to animes model into genres app

--- bunnoon/genres/models.py
+++ bunnoon/genres/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from animes.models import Anime
 
 # Create your models here.
 
@@ -7,3 +8,8 @@ class Genre(models.Model):
     handle = models.CharField(max_length=40, unique=True)
     enName = models.CharField(max_length=40)
     thName = models.CharField(max_length=40)
+
+
+class GenresToAnimes(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
