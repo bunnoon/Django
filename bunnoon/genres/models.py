ee425071d002cbf10e8bcd66a79da277f2de1aa1
Phuchon Chueapong <140912336+bunnoon@users.noreply.github.com>
2023-08-01 17:12:10 +0700

    update models in genres app
    create __str__ function all models

--- bunnoon/genres/models.py
+++ bunnoon/genres/models.py
@@ -9,7 +9,13 @@ class Genre(models.Model):
     enName = models.CharField(max_length=40)
     thName = models.CharField(max_length=40)
 
+    def __str__(self):
+        return self.enName
+
 
 class GenresToAnimes(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return '{} <-> {}'.format(self.genre.enName, self.anime.enName)
